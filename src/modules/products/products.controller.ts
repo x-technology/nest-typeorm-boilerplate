88c27842634812399c22ff4506993efc31894d4d
Denis Volper <denisvolper@gmail.com>
2022-07-30 11:42:01 +0300

    Added swagger documentation

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,16 +1,20 @@
-import { Controller } from "@nestjs/common";
-import { Crud, CrudController } from "@nestjsx/crud";
+import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
 
-import { Product } from "./entities/product.entity";
-import { ProductsService } from "./products.service";
+import { Product } from './entities/product.entity';
+import { ProductsService } from './products.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Crud({
   model: {
     type: Product,
   },
+  query: {
+    alwaysPaginate: true,
+  },
 })
-@Controller("products")
+@ApiTags('products')
+@Controller('products')
 export class ProductsController implements CrudController<Product> {
-  constructor(public service: ProductsService) {
-  }
+  constructor(public service: ProductsService) {}
 }
